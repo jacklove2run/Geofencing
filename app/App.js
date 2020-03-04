a8bd0940356d0314f463575af4bf197076fcdb0a
jacklove2run <529842912@qq.com>
2020-03-04 21:41:11 +0800

    GeoFencing code commit

--- app/App.js
+++ app/App.js
@@ -1,41 +1,108 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, BackHandler, Alert } from 'react-native';
 
 import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import * as TaskManager from 'expo-task-manager';
 import React from 'react';
 import axios from 'axios';
 
+const GEOFENCING_TASK = 'background-location-task';
+const regions = [{
+    'latitude': 38.6518,
+    'longitude': 104.07642,
+    'radius': 100,
+    'notifyOnEnter': true,
+    'notifyOnExit': true
+  }];
+
 async function report() {
-  const serverAddress = "40.118.175.173";
-  const serverPort = 3000;
+  const serverAddress = "40.118.175.173:3000";
   try {
-    const res = await axios.get(`http://${serverAddress}:${serverPort}/report/${Constants.deviceId}`)
-    if (res.status === 201) {
-      alert("Created!");
-    } else {
-      alert(`Error: status=${res.status}`);
-    }
+    alert(Constants.deviceId);
+    const res = await axios.get(`http://${serverAddress}/report/${Constants.deviceId}`)
+    console.log(res.data);
   } catch (error) {
     alert(error);
   }
 }
 
+async function getLocationAsync() {
+  let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  console.log(status);
+  if (status !== 'granted') {
+    return;
+  } else {
+    alert('Permissions granted');
+    return;
+  }
+  let { location } = await Location.getCurrentPositionAsync({}); //This code doesn't work on my device.
+  console.log('location ' + JSON.stringify(location));
+}
+
+async function checkStatus() {
+  let tasks = await TaskManager.getRegisteredTasksAsync(GEOFENCING_TASK);
+  console.log('registered tasks: ' + JSON.stringify(tasks));
+  let enable = await Location.hasServicesEnabledAsync();
+  console.log('services enable: ' + JSON.stringify(enable));
+  let last_location = await Location.getProviderStatusAsync();
+  console.log('last_location: ' + JSON.stringify(last_location));
+  let started = await Location.hasStartedGeofencingAsync(GEOFENCING_TASK);
+  console.log(started);
+}
+
 function registerBackgroundTask() {
   // Please follow this document to implement the background task
   // https://docs.expo.io/versions/latest/sdk/location/#geofencing-methods
-  alert("TODO: register background task for geoFencing, when device is in GPS range, call service API");
+  console.log("TODO: register background task for geoFencing, when device is in GPS range, call service API");
+  getLocationAsync();
+  try {
+    Location.startGeofencingAsync(GEOFENCING_TASK, regions);
+  } catch(error) {
+    console.log(error);
+  }
+  checkStatus();
 }
 
+TaskManager.defineTask(GEOFENCING_TASK, ({ data: { eventType, region }, error }) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (eventType === Location.GeofencingEventType.Enter) {
+    report();
+    console.log("You've entered region:", region);
+  } else if (eventType === Location.GeofencingEventType.Exit) {
+    console.log("You've left region:", region);
+  }
+});
+
+
+
+
+
 export default function App() {
+  BackHandler.addEventListener('hardwareBackPress', function() {
+    Alert.alert(
+      'Notice',
+      "Exit App?",
+      [
+        {text: 'OK', onPress: () => BackHandler.exitApp(), style: 'cancel'},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed')},
+      ]);
+    });
   return (
     <View style={styles.container}>
       <Button
-        title="Register" onPress={() => registerBackgroundTask()} />
+        title="Register now!!!!!!" onPress={() => registerBackgroundTask()} />
       <View style={{ height: 10 }} />
       <Button
         title="[Test] Call service API directly" onPress={() => report()} />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
--- service/index.js
+++ service/index.js
@@ -3,12 +3,31 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!');
 });
 
 app.get('/report/:deviceId', (req, res) => {
+	// TODO: Write timestamp + deviceId to MySQL database
+	mysql  = require('mysql');
+	var connection = mysql.createConnection({     
+	  host     : '127.0.0.1',       
+	  user     : 'root',              
+	  password : 'hhpPfkuF3ApfdD7z',       
+	  port: '3306',                   
+	  database: 'my_project' 
+	});
+	connection.connect();
+	var sql = 'insert into t_geofencing_report(id, deviceId, create_time) values (0, ?, ?)';
+	var sqlParam = [req.params.deviceId, parseInt(new Date().getTime()/1000)];
     console.log(`Report home: ${req.params.deviceId}`);
-    // TODO: Write timestamp + deviceId to MySQL database
+    connection.query(sql, sqlParam, function (err, result) {
+    	res.send(result);
+        if(err){
+        	console.log('[INSERT ERROR] - ',err.message);
+        	return;
+        }
+	});
+	connection.end();
     res.sendStatus(201);
 });
 
